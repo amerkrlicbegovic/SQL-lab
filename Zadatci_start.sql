b8f5440ca455e406d0e4c2760cbb244df6d95d06
Amer <amer.krlicbegovic@gmail.com>
2018-05-18 00:53:25 +0200

    Added the exercise

---
+++ Zadatci_start.sql
@@ -0,0 +1,172 @@
+﻿--ZADACI ZA VJEŽBU
+--Sve zadatke riješite koristeći bazu Fakultet.
+
+--ZADATAK ZA VJEŽBU 1.
+--Ispišite matični broj, ime i prezime svih studenata. Ime i prezime se trebaju
+--ispisati zajedno, u jednom stupcu.
+
+
+
+--ZADATAK ZA VJEŽBU 2.
+--Ispišite sva imena studenata tako da se niti jedno ime ne pojavi dva puta.
+--Sortirajte imena silazno po abecedi.
+
+
+
+--ZADATAK ZA VJEŽBU 3.
+--Ispišite matične brojeve studenata koji su položili ispit iz predmeta sa šifrom 146.
+
+
+
+--ZADATAK ZA VJEŽBU 4.
+--Ispišite ime i prezime nastavnika te plaću dobivenu formulom: (koeficijent + 0.4) * 800.
+
+
+
+--ZADATAK ZA VJEŽBU 5.
+--Ispišite imena i prezimena nastavnika iz prethodnog zadatka koji imaju
+--plaću manju od 3500 ili veću od 8000.
+
+
+
+--ZADATAK ZA VJEŽBU 6.
+--Ispišite imena i prezimena studenata koji su barem jednom pali na ispitu iz
+--predmeta sa šiframa od 220 do 240.
+
+
+
+--ZADATAK ZA VJEŽBU 7.
+--Ispišite imena i prezimena studenata koji su na nekom ispitu dobili 3.
+
+
+--ZADATAK ZA VJEŽBU 8.
+--Ispišite nazive predmeta na koje niti jedan student nije izašao.
+
+
+--ZADATAK ZA VJEŽBU 9.
+--Ispišite nazive predmeta na koje je izašao barem jedan student.
+
+
+--ZADATAK ZA VJEŽBU 10.
+--Ispišite sve podatke o studentima kojima ime počinje samoglasnikom.
+
+
+--ZADATAK ZA VJEŽBU 11.
+--Ispišite sve podatke o studentima kojima ime završava bilo kojim
+--znakom osim samoglasnikom.
+
+
+--ZADATAK ZA VJEŽBU 12.
+--Ispišite sve podatke o studentima kojima ime završava samoglasnikom.
+
+
+--ZADATAK ZA VJEŽBU 13.
+--Ispišite sve podatke o studentima kojima ime bilo gdje sadrži
+--slova n i k jedno iza drugog.
+
+
+--ZADATAK ZA VJEŽBU 14.
+--Ispišite ime i prezime studenta, te naziv i ocjenu za svaki ispit.
+
+
+--ZADATAK ZA VJEŽBU 15.
+--Ispišite ime i prezime studenta, te mjesto i županiju u kojoj su rođeni.
+--Nakon toga dodajte mjesto i županiju u kojoj stanuju.
+
+ 
+--ZADATAK ZA VJEŽBU 15.
+--Ispišite nazive kolegija i zavode kojima pripadaju za one kolegije koji imaju
+--više od 20 upisanih studenata.
+
+
+
+--ZADATAK ZA VJEŽBU 17.
+--Ispišite sve nazive mjesta u kojima barem jedan student stanuje. Imena
+--mjesta sa smiju pojaviti samo jednom.
+
+
+--ZADATAK ZA VJEŽBU 18.
+--Ispišite nazive mjesta u kojima je barem jedan student i rođen i stanuje.
+--Imena mjesta smiju se pojaviti samo jednom.
+
+
+ --ZADATAK ZA VJEŽBU 19.
+--Ispišite oznake dvorana i nazive kolegija za one kolegije koji imaju više od
+--2 sata predavanja tjedno.
+
+
+
+ --ZADATAK ZA VJEŽBU 20.
+--Ispišite nazive kolegija i oznake dvorana u kojima imaju rezerviran termin.
+--Ispišite i naziv organizacijske jedinice kojoj kolegij pripada.
+
+
+--ZADATAK ZA VJEŽBU 21.
+--Ispišite podatke o nastavnicima koji su ispitivali studente koji stanuju u istoj
+--županiji u kojoj stanuje i nastavnik
+
+
+--ZADATAK ZA VJEŽBU 22.
+--Ispišite podatke o studentima koji studiraju u mjestu različitom od mjesta
+--rođenja, ali koje se nalazi u istoj županiji gdje su i rođeni.
+
+
+
+--ZADATAK ZA VJEŽBU 23.
+--Ispišite podatke o studentima i nastavnicima koji imaju ista prezimena.
+
+
+
+--ZADATAK ZA VJEŽBU 24.
+--Ispišite podatke o studentima i njihovim mjestima stanovanja za one
+--studente koji su bar jednom pali na ispitu 'Osnove baza podataka'
+
+
+
+--ZADATAK ZA VJEŽBU 25.
+--Ispišite ime i prezime nastavnika, mjesto stanovanja i županiju nastavnika
+--te naziv kolegija za nastavnike koji su ispitivali studente koji su dobili ocjenu
+--2 ili 3 iz tog kolegija.
+
+
+
+--5.5.1 ZADACI ZA VJEŽBU	--										
+--ZADATAK ZA VJEŽBU 26.
+--Ispišite ime i prezime te mjesto rođenja studenata kojima ime počinje
+--slovom F
+
+
+--ZADATAK ZA VJEŽBU 27.
+--Ispišite ime i prezime nastavnika koji živi u mjestu koje na trećem mjestu
+--ima slovo Z.
+
+												
+ 
+--ZADATAK ZA VJEŽBU 28.
+--Ispišite ime i prezime studenata te imena i prezimena nastavnika koji u
+--imenu imaju na petom mjestu isto slovo.
+
+
+
+--ZADATAK ZA VJEŽBU 29.
+--Ispišite nazive županija kojima je duljina imena duža od 13 znakova a kraća
+--od 20 znakova.
+
+
+
+--ZADATAK ZA VJEŽBU 30.
+--Ispišite ime i prezime studenata koji su rođeni u svibnju.
+
+ 
+
+--ZADATAK ZA VJEŽBU 31.
+--Ispišite ime i prezime studenata koji su na ispit izašli između 10 i 20 u
+--mjesecu.
+
+
+
+--ZADATAK ZA VJEŽBU 32*.
+--Pronađite podatke o onim nastavnicima koji su u lipnju ispitivali studente
+--čije je mjesto rođenja isto mjestu gdje stanuje nastavnik. Ispišite ime i
+--prezime nastavnika, naziv mjesta gdje stanuje, ime i prezime studenta
+
